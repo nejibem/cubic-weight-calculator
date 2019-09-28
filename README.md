e3a6e1e036212665b560cd3255264e0cb5872283
benjamin mooney <me@nejibem.io>
2019-09-28 15:32:00 +1000

    Complete coding challenge requirements

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cubic-weight-calculator
-Cubic Weight Calculator
+# Cubic Weight Calculator
+
+## Running Application
+1) Clone repository `git clone git@github.com:nejibem/cubic-weight-calculator.git`
+2) Change into dir `cd cubic-weight-calculator`
+3) Install dependencies `yarn install`
+4) Run app `yarn start`
+5) Browser should automatically launch react page in browser at `http://localhost:3000/`
--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com';
+const PRODUCTS_ENDPOINT = '/api/products/1';
+const CUBIC_WEIGHT_CONVERSION_FACTOR = 250;
+
+class App extends React.Component {
+  state = {
+    loading: false,
+    allProducts: [],
+  };
+
+  componentDidMount() {
+    this.loadProducts(API_URL, PRODUCTS_ENDPOINT);
+  }
+
+  loadProducts = (apiUrl, endpoint) => {
+    fetch(apiUrl + endpoint)
+      .then(res => res.json())
+      .then(data => {
+        this.setState((prevState) => ({
+          allProducts: prevState.allProducts.concat(data.objects),
+        }));
+        if(data.next) {
+          this.loadProducts(apiUrl, data.next);
+        }
+      });
+  };
+
+  filterByCategory = (products, category) => {
+    return products.filter(product => product.category === category)
+  };
+
+  calculateCubicWeight = (product) => {
+    const { size: { width, length, height } } = product;
+    return (length/100) * (width/100) * (height/100) * CUBIC_WEIGHT_CONVERSION_FACTOR;
+  };
+
+  calculateCubicWeightsForProducts = (products) => {
+    return products.map(product => ({
+      ...product,
+      cubicWeight: this.calculateCubicWeight(product),
+    }));
+  };
+
+  totalProductsWeight = (acc, cur) => {
+    return acc + cur.cubicWeight;
+  };
+
+  render() {
+    const { allProducts } = this.state;
+
+    const airConditioners = allProducts.length
+      ? this.filterByCategory(allProducts, "Air Conditioners")
+      : [];
+
+    const airConditionersWithWeights = allProducts.length
+      ? this.calculateCubicWeightsForProducts(airConditioners)
+      : [];
+
+    const averageAirConditionerWeight = allProducts.length
+      ? airConditionersWithWeights.reduce(this.totalProductsWeight, 0) / airConditionersWithWeights.length
+      : 0;
+
+    return(
+      <div className="App">
+        <h1>Cubic Weight Calculator</h1>
+        <h2>Air Conditioners</h2>
+        <div>
+          <ul>
+            {airConditionersWithWeights.map((airCon) => (
+                <li key={airCon.title}>{airCon.title} - {airCon.cubicWeight.toFixed(2)}kg</li>)
+            )}
+          </ul>
+          <div>
+            Average AirConditioner Weight: {averageAirConditionerWeight.toFixed(2)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
